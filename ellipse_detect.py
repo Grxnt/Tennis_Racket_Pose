fcd68343d44e9470a2d1fd2117b79e7b4c06e774
Eric Li <60822038+ericli54@users.noreply.github.com>
2022-11-30 19:01:31 -0600

    Grant is scared he's going to screw everything up

--- ellipse_detect.py
+++ ellipse_detect.py
@@ -8,21 +8,29 @@ from skimage.transform import hough_ellipse
 from skimage.draw import ellipse_perimeter
 
 # Load picture, convert to grayscale and detect edges
-img = cv2.imread("images/circles.jpg")[0:320, 0:320]#data.coffee()[0:220, 160:420]
+img = cv2.imread("images/racket_tilt.jpg")#data.coffee()[0:220, 160:420]
 #image_gray = cv2.cvtColor(image_rgb, cv2.COLOR_BGR2GRAY)
+scale_percent = 300 / len(img)# percent of original size
+width = int(img.shape[1] * scale_percent)
+height = int(img.shape[0] * scale_percent)
+dim = (width, height)
+  
+# resize image
+img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+
 gray = img[:,:,0]*0.1 + img[:,:,1]*0.8 + img[:,:,2]*0.1
 gray = np.uint8(gray)
-gray = cv2.medianBlur(gray,5)
-cv2.imwrite('idk.jpg', gray)
-edges = canny(gray, sigma=2.0,
-              low_threshold=0.55, high_threshold=0.8)
+gray = cv2.GaussianBlur(gray,(5,5),5)
+edges = canny(gray)
+cv2.imwrite('images/edges.jpg', 255*np.uint8(edges))
+
 
 # Perform a Hough Transform
 # The accuracy corresponds to the bin size of a major axis.
 # The value is chosen in order to get a single high accumulator.
 # The threshold eliminates low accumulators
 result = hough_ellipse(edges, accuracy=20, threshold=250,
-                       min_size=10, max_size=500)
+                       min_size=80, max_size=120)
 result.sort(order='accumulator')
 
 # Estimated parameters for the ellipse
